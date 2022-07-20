dc63a4595bc5d5d894a4b174c90933264957cf72
Deivys <deivysgoncalves@gmail.com>
2022-07-20 10:07:57 -0300

    Alterando a mensagem de posts não encontrados

--- plugins/eltd-listing/single/templates/related-posts/no-post-found.php
+++ plugins/eltd-listing/single/templates/related-posts/no-post-found.php
@@ -1,3 +1,3 @@
 <p class="eltd-ls-not-found">
-    <?php esc_html_e( 'Sorry, no posts matched your criteria.', 'eltd-listing' ); ?>
+    <?php //esc_html_e( 'Sorry, no posts matched your criteria.', 'eltd-listing' ); ?>
 </p>
